a39f266be6ad9eaa86e77f2f9987e27584f5dc4c
MLC\david.wu <mooneve95525@gmail.com>
2025-12-31 11:04:23 +0800

    20251231 -port刪除

--- index.js
+++ index.js
@@ -1,20 +1,5 @@
 require("dotenv").config();
 /* =========================
-   Express（給 Render 掃 port）
-========================= */
-const express = require("express");
-const app = express();
-const port = process.env.PORT || 4000;
-
-app.get("/", (req, res) => {
-  res.send("Discord bot is running");
-});
-
-app.listen(port, () => {
-  console.log(`🌐 HTTP server listening on port ${port}`);
-});
-
-/* =========================
    Discord Bot
 ========================= */
 const { Client, GatewayIntentBits, EmbedBuilder } = require("discord.js");
