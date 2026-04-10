4a622f9d6f6b8a2614141f69c109343735339e09
MLC\david.wu <mooneve95525@gmail.com>
2026-04-10 08:40:01 +0800

    fix:!小雞 指令修改連結

--- commands.js
+++ commands.js
@@ -1,18 +1,18 @@
-const {response} = require("express");
+const { response } = require("express");
 
 module.exports = [
   // === 文字回覆 ===
-  {trigger: "!去留", response: "(′・ω・‵) 該考慮去留了"},
-  {trigger: "!下班", response: "(′・ω・‵) 走ㄌ走ㄌ"},
-  {trigger: "!震怒", response: "(′・ω・‵) 嘎喔"},
+  { trigger: "!去留", response: "(′・ω・‵) 該考慮去留了" },
+  { trigger: "!下班", response: "(′・ω・‵) 走ㄌ走ㄌ" },
+  { trigger: "!震怒", response: "(′・ω・‵) 嘎喔" },
 
-  {trigger: "!欺騙", response: "(′・ω・‵) 大家都被欺騙感情丁欺騙感情"},
-  {trigger: "!爽丁", response: "欺騙感情丁：今天撿到免費炒麵，潮爽DER"},
-  {trigger: "!破防", response: "(′ Q ω Q ‵)"},
-  {trigger: "!尻尻", response: "尻尻：雅狗欠我狗爸摳"},
-  {trigger: "!中", response: "噫！好了！我中了！"},
-  {trigger: "!仲裁", response: "紅茶：你們都有罪"},
-  {trigger: "!大貓", response: "(^・ω・^=)"},
+  { trigger: "!欺騙", response: "(′・ω・‵) 大家都被欺騙感情丁欺騙感情" },
+  { trigger: "!爽丁", response: "欺騙感情丁：今天撿到免費炒麵，潮爽DER" },
+  { trigger: "!破防", response: "(′ Q ω Q ‵)" },
+  { trigger: "!尻尻", response: "尻尻：雅狗欠我狗爸摳" },
+  { trigger: "!中", response: "噫！好了！我中了！" },
+  { trigger: "!仲裁", response: "紅茶：你們都有罪" },
+  { trigger: "!大貓", response: "(^・ω・^=)" },
   {
     trigger: "!爽尻",
     response: [
@@ -66,6 +66,11 @@ module.exports = [
     response:
       "https://cdn.discordapp.com/attachments/1425749836321390632/1425755083039182889/584548878.png?ex=68e8bd65&is=68e76be5&hm=5dd05c53bde05d8e2279fe504fb31e707bac0be9cde1ef280d7423cec89dd5cd&",
   },
+  {
+    trigger: "!賽亞狗",
+    response:
+      "https://cdn.discordapp.com/attachments/1282970244603842560/1491687278467088424/image.png?ex=69d89989&is=69d74809&hm=a464d56c592037e9cdd010f1112335c7730a8285d9b745902312b79b1c473d2f&",
+  },
 
   {
     trigger: "!等",
@@ -116,7 +121,7 @@ module.exports = [
     response:
       "https://cdn.discordapp.com/attachments/1425749836321390632/1439870927822258176/image.png?ex=691c17cc&is=691ac64c&hm=e7ad2bc2a1e65dc04e0a59d2c965c38a1c75df3293b28ac51030c569edcc8928&",
   },
-  {trigger: "!你婆", response: "https://i.imgur.com/YDzW7ZT.jpg"},
+  { trigger: "!你婆", response: "https://i.imgur.com/YDzW7ZT.jpg" },
   {
     trigger: "!計算機",
     response:
@@ -182,8 +187,8 @@ module.exports = [
     response:
       "https://cdn.discordapp.com/attachments/1271056903350255646/1271734977842778153/ezgif-6-1c5871195afe_1.gif?ex=68eea73e&is=68ed55be&hm=fc47c947c6a61a69a09a743c3744e13fae0ae85ffb33a5438b877e43c4d59b73&",
   },
-  {trigger: "!贖", response: "https://imgur.com/DumSMmE"},
-  {trigger: "!二口", response: "https://imgur.com/TX8FRcJ"},
+  { trigger: "!贖", response: "https://imgur.com/DumSMmE" },
+  { trigger: "!二口", response: "https://imgur.com/TX8FRcJ" },
   {
     trigger: "!一發入魂",
     response:
@@ -236,7 +241,7 @@ module.exports = [
     response:
       "https://cdn.discordapp.com/emojis/1115533369925713922.webp?size=160&animated=true",
   },
-  {trigger: "!YOHO", response: "https://imgur.com/QhOzdko"},
+  { trigger: "!YOHO", response: "https://imgur.com/QhOzdko" },
   {
     trigger: "!窮醒",
     response:
@@ -519,7 +524,7 @@ module.exports = [
       "https://media.tenor.com/dJl050ZdO5sAAAAC/%E3%83%9B%E3%83%AD%E3%83%A9%E3%82%A4%E3%83%96-hololive.gif",
       "https://cdn.discordapp.com/attachments/826121054086627388/1411909015645978665/1411829549972590705-ConvertToGIF0.gif?ex=69ce84fd&is=69cd337d&hm=f895524fce3418a1dc10975c418be1cb8a11b23958d50490fb68a51fc5cc7b83&",
       "https://media.discordapp.net/attachments/950412894548414537/1358397133404766299/GnzSZ7YacAAupq-.gif?width=525&height=525&ex=69cef62a&is=69cda4aa&hm=4246c291f4ed890c39ddfe86afba0b9c8979a04759a6dc9fcb8d8cf7eeb4d8c6&",
-      "https://tenor.com/view/%E9%9F%B3%E4%B9%83%E7%80%AC%E5%A5%8F-%E5%A5%8F-otonose-kanade-kanade-hololive-devis-gif-2385180011681261720",
+      "https://tenor.com/c0mjrPM4A9Q.gif",
       "https://imgur.com/z9WdpGz"
     ]
   },
